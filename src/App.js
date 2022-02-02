7a8862b9d6876295ec86946e139130b25c6d2879
= <76081373+tamaraafvieira@users.noreply.github.com>
2022-02-02 20:23:51 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./componentes/Home";
+import Contato from "./componentes/Contato";
+import Destino from "./componentes/Destino";
+import Promocao from "./componentes/Promocao";
+import Clientes from "./componentes/Clientes";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 function App() {
+
+  const url = window.location.href;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {       
+        url === 'http://localhost:3000/Home' 
+        ? <Home/>
+        : url === 'http://localhost:3000/Destino' 
+        ? <Destino/>
+        : url === 'http://localhost:3000/Promocao' 
+        ? <Promocao/>
+        : url === 'http://localhost:3000/Contato' 
+        ? <Contato/>        
+        : url === 'http://localhost:3000/Clientes'        
+        ? <Clientes/>        
+        : 'Erro'
+      }
     </div>
   );
 }
---
+++ src/componentes/Clientes/index.js
@@ -0,0 +1,133 @@
+import React from "react";
+import Header from "../../componentegenerico/Header";
+import Footer from "../../componentegenerico/Footer";
+import "./style.css";
+
+function Clientes () {
+    return (
+        <div>
+            <Header/>
+                <div class="clientes">
+                <h2>Lista de clientes cadastrados.</h2>
+                <br/>    
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10">
+                                <h2>Clientes Cadastrados</h2>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>CodigoCliente</th>
+                                            <th>Nome</th>
+                                            <th>Cpf</th>
+                                            <th>E_mail</th>
+                                            <th>Rua</th>
+                                            <th>Numero</th>
+                                            <th>Complemento</th>
+                                            <th>Cep</th>
+                                        </tr>
+                                    </thead>                                    
+                                </table>                                
+                            
+                                <hr />
+                                <h2>Cadastrar</h2>
+                                <form asp-controller="Cliente" asp-action="Index">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="">Nome completo</label>
+                                            <input asp-for="Nome" type="text" class="form-control" id="" placeholder="Nome Completo"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Cpf</label>
+                                            <input asp-for="Cpf" type="text" class="form-control" id="" placeholder="000.000.000-00"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">E_mail</label>
+                                            <input asp-for="E_mail" type="text" class="form-control" id="" placeholder="e-mail"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Rua</label>
+                                            <input asp-for="Rua" type="text" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Numero</label>
+                                            <input asp-for="Numero" type="int" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Complemento</label>
+                                            <input asp-for="Complemento" type="text" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Cep</label>
+                                            <input asp-for="Cep" type="text" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Situação</label>
+                                            <select class="custom-select" asp-for="Situacao">
+                                                <option selected>-- Selecione uma opção</option>
+                                                <option>Ativo</option>
+                                                <option>Inativo</option>
+                                                <option>Bloqueado</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-success">Incluir</button>
+                                </form>
+                                <br />
+                                <hr />
+                                <br />
+                                <h2>Atualizar Dados</h2>
+                                <form asp-controller="Cliente" asp-action="Atualizar">
+
+                                    <div class="form-row">
+                                        <div class="form-group col-md-3">
+                                            <label for="">Código do cliente</label>
+                                            <select class="custom-select" asp-for="CodigoCliente">
+                                                <option selected>-- Selecione um código</option>                                                
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="">Nome completo</label>
+                                            <input asp-for="Nome" type="text" class="form-control" id="" placeholder="Nome Completo"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">CPF</label>
+                                            <input asp-for="Cpf" type="text" class="form-control" id="" placeholder="000.000.000-00"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">E_mail</label>
+                                            <input asp-for="E_mail" type="text" class="form-control" id="" placeholder="e-mail"/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Rua</label>
+                                            <input asp-for="Rua" type="text" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Numero</label>
+                                            <input asp-for="Numero" type="int" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Complemento</label>
+                                            <input asp-for="Complemento" type="text" class="form-control" id=""/>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">Situação</label>
+                                            <select class="custom-select" asp-for="Situacao">
+                                                <option selected>-- Selecione uma opção</option>
+                                                <option>Ativo</option>
+                                                <option>Inativo</option>
+                                                <option>Bloqueado</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Atualizar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+            <Footer/>
+        </div>
+    );
+}
+export default Clientes;
